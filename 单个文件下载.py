c96160dfc3eddbf166ccfb5dffdca7ba760367f0
xytx <3232076199@qq.com>
2024-01-05 10:51:33 +0800

    删除误提交的文件

--- 单个文件下载.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    :2023/12/27 18:38
-# @Author  :小 y 同 学
-# @公众号   :小y只会写bug
-# @CSDN    :https://blog.csdn.net/weixin_64989228?type=blog
-
-import NewSentinelDownload as sd
-if __name__ == '__main__':
